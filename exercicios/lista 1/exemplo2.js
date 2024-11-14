b3d04432fd8654f58978867c522d291e57edd9fb
Pamell <senacpami@gmail.com>
2024-11-14 16:10:38 -0300

    exercicios resolvidos

---
+++ exercicios/lista 1/exemplo2.js
@@ -0,0 +1,4 @@
+
+A = Number (prompt ("Qual o primeiro número?"));
+B = Number (prompt ("Qual o primeiro número?"));
+document.write(` A multiplicação entre  ${A}   e   ${B}   é   ${resultado = A * B}.`);
---
+++ exercicios/lista 1/exemplo5.js
@@ -0,0 +1,7 @@
+ A = Number(prompt("Qual o primeiro número?"));
+ B = Number( prompt("Qual o segundo número?"));
+ C = Number (prompt("Qual o terceiro número?"));
+ document.write(` A mediana entre  ${A}   e   ${B}   e  ${C} é ${resultado = (A + B + C)/3 }.`);
+
+
+
