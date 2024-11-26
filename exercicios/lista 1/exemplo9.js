c6439c3bb8037018ec0877a083264d966cbf6ce6
Pamell <senacpami@gmail.com>
2024-11-26 17:24:05 -0300

    exemplo9.js

---
+++ exercicios/lista 1/exemplo9.js
@@ -0,0 +1,17 @@
+var carro1 = {
+
+marca:"VW",
+modelo: "Fusca",
+ano: 1959,
+classe: "Passeio" ,
+cor: "Preta",
+cambio: "manual 6 marcha",
+direçao: "Hidraulica",
+calibragem: { dianteiro: 32, traseiro: 32, step: 36}
+}
+console.log(carro1);
+console.table(carro1);
+
+
+
+
