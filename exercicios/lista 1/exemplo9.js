386104bd6abae6f836321f20df73e2334bf9d7bf
Pamell <senacpami@gmail.com>
2024-11-27 17:27:24 -0300

    exemplo9.js

--- exercicios/lista 1/exemplo9.js
+++ exercicios/lista 1/exemplo9.js
@@ -1,17 +1,35 @@
-var carro1 = {
 
-marca:"VW",
-modelo: "Fusca",
-ano: 1959,
-classe: "Passeio" ,
-cor: "Preta",
-cambio: "manual 6 marcha",
-direçao: "Hidraulica",
-calibragem: { dianteiro: 32, traseiro: 32, step: 36}
+var Fusca = {
+    
+    marca:"VW",
+    modelo: "Fusca",
+    ano: 1959,
+    classe: "Passeio" ,
+    cor: "Preta",
+    cambio: "manual 6 marcha",
+    direçao: "Hidraulica",
+    velocidade: 0,
+    calibragem: { dianteiro: 32, traseiro: 32, step: 36}
 }
-console.log(carro1);
-console.table(carro1);
 
+function acelerar(carro){
+    carro.velocidade += 5;   
+}
+function frear(carro){
+    Fusca.velocidade -=13;
+
+    if(carro.velocidade < 0){
+        carro.velocidade = 0;
+    }
+    
+}
+while(Fusca.velocidade < 80)acelerar(Fusca);
+console.log(Fusca);
+while(Fusca.velocidade >= 60 )frear();
+console.log(Fusca);
+while(Fusca.velocidade > 0 )frear();
+console.log(Fusca);
+console.table(Fusca);
 
 
 
