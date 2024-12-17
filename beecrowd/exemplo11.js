953e261c8e29f8244682f176e011cc5c9f0ab2c7
Pamell <senacpami@gmail.com>
2024-12-17 15:08:56 -0300

    exercicios da lista1

---
+++ beecrowd/exemplo11.js
@@ -0,0 +1,10 @@
+// var input = require('fs').readFileSync('./input.txt', 'utf8');
+// var lines = input.split('\n');
+// var A = Number(lines[0])
+// var B = Number(lines[1])
+// var SOMA = (A + B)
+// console.log(SOMA)
+
+
+
+
---
+++ exercicios/exemplo10.js
@@ -0,0 +1,20 @@
+var frase = "O rato roeu a roupa do rei de Roma";
+var palavras = frase.split(' ');
+
+console.log(frase)
+console.log(palavras)
+
+var frase = "JavaScript é muito fácil!";
+var palavras = frase.split(' ');
+
+console.log(frase)
+console.log(palavras)
+
+var frase = "Eu gosto de programar";
+var palavras = frase.split(' ');
+
+console.log(frase)
+console.log(palavras)
+
+
+
