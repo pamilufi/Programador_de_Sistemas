757ad5cdfc3f255c2751036b7fb4855cda0259fb
Pamell <senacpami@gmail.com>
2024-12-17 16:35:00 -0300

    resolução da lista 3.1

---
+++ exercicios/lib/lista3.js
@@ -0,0 +1,12 @@
+export function prova(nota) {
+    if (nota >= 7) {
+        return "Aprovado"
+    }
+    return "Reprovado"
+}
+
+export function parOuImpar(numero) {
+    return "Par"
+
+
+}
---
+++ exercicios/testes/exercicio3.1.js
@@ -0,0 +1,8 @@
+import { prova } from "../lib/lista3.js";
+
+console.log(prova(10))
+console.log(prova(7.0))
+console.log(prova(6.95))
+console.log(prova(8.8))
+console.log(prova(0))
+console.log(prova(2.725))
