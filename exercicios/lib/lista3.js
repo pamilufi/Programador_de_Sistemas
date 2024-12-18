6aac7e8fb1d0bcbe4bc680d63401e863df0552f6
pamilufi <senacpami@gmail.com>
2024-12-18 17:20:01 -0300

    resolução exercicio 4 lista 3

--- exercicios/lib/lista3.js
+++ exercicios/lib/lista3.js
@@ -1,30 +1,46 @@
 export function prova(nota) {
-    if (nota >= 7) {
-        return "Aprovado"
-    }
-    return "Reprovado"
+  if (nota >= 7) {
+    return "Aprovado"
+  }
+  return "Reprovado"
 }
 
 export function parOuImpar(numero) {
-    if (numero % 2 === 0) {
-        return "Par"
+  if (numero % 2 === 0) {
+    return "Par"
 
-    }
-    return "Impar"
+  }
+  return "Impar"
 
 
 }
 
-export function positivoOuNegativo (numero) {
-    const numeroConvertido = Number(numero);
-  
-    if (isNaN(numeroConvertido)) {
-      return "Entrada inválida: não é um número";
-    } else if (numeroConvertido > 0) {
-      return "Positivo";
-    } else if (numeroConvertido < 0) {
-      return "Negativo";
-    } else {
-      return "Zero";
-    }
+export function positivoOuNegativo(numero) {
+  const numeroConvertido = Number(numero);
+
+  if (isNaN(numeroConvertido)) {
+    return "Entrada inválida: não é um número";
+  } else if (numeroConvertido > 0) {
+    return "Positivo";
+  } else if (numeroConvertido < 0) {
+    return "Negativo";
+  } else {
+    return "Zero";
   }
+}
+
+
+export function menorNumero(numero1, numero2, numero3) {
+  let menor = numero1
+  if (numero3 < menor) {
+
+  }
+  if (numero2 < menor) {
+    menor = numero2
+
+  }
+  if (numero3 < menor) {
+    menor = numero3
+  }
+  return menor;
+}
---
+++ exercicios/testes/exercicio3.4.js
@@ -0,0 +1,5 @@
+import { menorNumero } from "../lib/lista3.js";
+
+console.log(menorNumero(3,7,5))
+console.log(menorNumero(-1,-5,-3))
+console.log(menorNumero(8,2,10))
