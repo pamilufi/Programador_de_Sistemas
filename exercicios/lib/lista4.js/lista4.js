471b57f27837cbf1e944b074f365a64a64451246
pamilufi <senacpami@gmail.com>
2024-12-20 13:55:05 -0300

    exercicio 3 lista 4

--- exercicios/lib/lista4.js/lista4.js
+++ exercicios/lib/lista4.js/lista4.js
@@ -12,3 +12,12 @@ const list3 = [5, 10, 15, 20, 25]
 
 const list4 = [5, 10, 15, 20, 25] 
 //   console.log(list4.indexOf(30));
+
+export const array1 = [1, 2, 3, 4, 5];
+console.log(array1.includes(3));
+
+const array2 = [10, 20, 30, 40, 50];
+console.log(array2.includes(25));
+
+const array3 = [100, 200, 300];
+console.log(array3.includes(100));
---
+++ exercicios/lib/lista4.js/teste/exercicio4.3.js
@@ -0,0 +1,5 @@
+import { array1 } from "../lista4.js";
+
+console.log(array1(1, 2, 3, 4, 5));
+console.log(array1(10, 20, 30, 40, 50));
+console.log(array1(100,200,300));
