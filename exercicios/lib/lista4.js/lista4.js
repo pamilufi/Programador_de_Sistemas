9dc1f6112dcb510af54c6f4e80f259e61c38cf9f
pamilufi <senacpami@gmail.com>
2024-12-20 13:52:11 -0300

    exercicio 2 lista 4

--- exercicios/lib/lista4.js/lista4.js
+++ exercicios/lib/lista4.js/lista4.js
@@ -1,1 +1,14 @@
 export var str = "";
+
+
+export const list1 = [1, 2, 3, 4, 5]
+// console.log(list1.indexOf(3));
+  
+const list2 = [10,20,30,40,50]
+// console.log(list2.indexOf(20));
+
+const list3 = [5, 10, 15, 20, 25]
+//   console.log(list3.indexOf(25));
+
+const list4 = [5, 10, 15, 20, 25] 
+//   console.log(list4.indexOf(30));
---
+++ exercicios/lib/lista4.js/teste/exercicio4.2.js
@@ -0,0 +1,6 @@
+import { list1 } from "../lista4.js";
+
+console.log(list1.indexOf(3));
+console.log(list2.indexOf(20));
+console.log(list3.indexOf(25));
+console.log(list4.indexOf(30));
