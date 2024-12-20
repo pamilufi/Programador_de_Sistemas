fc99f993dd49cbfd8061c18dc0b4f5749f4edfe8
pamilufi <senacpami@gmail.com>
2024-12-20 11:36:11 -0300

    resolução exercicio 3 lista 2

---
+++ exercicios/lib/lista2.js/teste/exercicio2.3.js
@@ -0,0 +1,3 @@
+import { somar } from "./lista2.js";
+
+  console.log(somar); 
--- exercicios/lib/lista2.js/teste/lista2.js
+++ exercicios/lib/lista2.js/teste/lista2.js
@@ -6,3 +6,14 @@ export function escreverOlaMundo() {
     let nome = prompt("Digite seu nome:");
     alert("Olá, " + Bruna + "!");
   }
+
+  export function somar(num1, num2) {
+    // Convertendo os valores para números, caso sejam strings
+    let resultado = Number(num1) + Number(num2);
+    return resultado;
+  }
+  
+  // Exemplos de uso:
+  let soma1 = somar(4, 5);
+  let soma2 = somar(10, 15);
+  let soma3 = somar("-3", "7");
