1b494c6d662e85c73dbb33c6f7f186d6559b5aeb
pamilufi <senacpami@gmail.com>
2024-12-20 10:25:03 -0300

    resolução exercicio 6 lista 3

--- exercicios/lib/lista3.js
+++ exercicios/lib/lista3.js
@@ -61,7 +61,4 @@ return maior;
 export function saoMultiplos (num1,num2){
 if (num1 === 0 || num2 === 0)
 }
-{
-return false;
-}
 return num1 % num2 === 0 || num2 %¨num1 ===0;
---
+++ exercicios/testes/exercicio3.6.js
@@ -0,0 +1,5 @@
+ import { saoMultiplos } from "../lib/lista3.js";
+
+ console.log(saoMultiplos(10,5));
+ console.log(saoMultiplos(12,7));
+ console.log(saoMultiplos(4,20));
