40564ac21f930cbbf48a06b76c27fe2ac47c777f
pamilufi <senacpami@gmail.com>
2024-12-20 15:02:21 -0300

    exercicio 1 lita 5

---
+++ exercicios/lib/lista5.js/lista5.js
@@ -0,0 +1,33 @@
+export var frase = "O rato roeu a roupa do rei de Roma";
+var palavras = frase.split(' ');
+
+var frase = "JavaScript é muito fácil!";
+var palavras = frase.split(' ');
+
+var frase = "Eu gosto de programar";
+var palavras = frase.split(' ');
+
+export var nomes = "João,Maria,José";
+var palavras = nomes.split(',');
+
+var nomes = "Ana Maria,Paulo Coelho,Carla";
+var palavras = nomes.split(',');
+
+
+export var email = "fulano@gmail.com;ciclano@yahoo.com;beltrano@outlook.com";
+var palavras = email.split(';');
+
+var email = "contato@empresa.com;suporte@site.com";
+var palavras = email.split(';');
+
+var frase = "admin@fundacao.org;info@site.org";
+var palavras = email.split(';');
+ 
+export var frase = "Nome -- Sobrenome -- Idade -- País";
+var palavras = frase.split(' -- ');
+
+var frase = "Item 1 -- Item 2 -- Item 3 -- Item 4";
+var palavras = frase.split(' -- ');
+
+var frase = "Rua A -- Bairro B -- Cidade C -- Estado D";
+var palavras = frase.split(' -- ');
