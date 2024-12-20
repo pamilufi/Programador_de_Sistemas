0ae826747c0bb6b73d04a33831638efe860d3618
pamilufi <senacpami@gmail.com>
2024-12-20 14:03:53 -0300

    exercicio 5 lista 4

--- exercicios/lib/lista4.js/lista4.js
+++ exercicios/lib/lista4.js/lista4.js
@@ -3,14 +3,14 @@ export var str = "";
 
 export const list1 = [1, 2, 3, 4, 5]
 // console.log(list1.indexOf(3));
-  
-const list2 = [10,20,30,40,50]
+
+const list2 = [10, 20, 30, 40, 50]
 // console.log(list2.indexOf(20));
 
 const list3 = [5, 10, 15, 20, 25]
 //   console.log(list3.indexOf(25));
 
-const list4 = [5, 10, 15, 20, 25] 
+const list4 = [5, 10, 15, 20, 25]
 //   console.log(list4.indexOf(30));
 
 export const array1 = [1, 2, 3, 4, 5];
@@ -34,3 +34,15 @@ var ultimo3Pop = ultimo3.pop();
 var ultimo4 = [];
 var ultimo4Pop = ultimo4.pop();
 
+
+export const array1 = [1, 2, 3, 4, 5];
+const firstElement = array1.shift();
+
+const array2 = ["a", "b", "c"];
+const firstElement2 = array2.shift();
+
+const array3 = [10];
+const firstElement3 = array3.shift();
+
+const array4 = [];
+const firstElement4 = array4.shift();
---
+++ exercicios/lib/lista4.js/teste/exercicio4.5.js
@@ -0,0 +1,10 @@
+import { array1 } from "../lista4.js";
+
+onsole.log(array1);
+console.log(firstElement);
+console.log(array2);
+console.log(firstElement2);
+console.log(array3);
+console.log(firstElement3);
+console.log(array4);
+console.log(firstElement4);
